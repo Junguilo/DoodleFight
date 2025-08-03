a63c184336fe4f69ed4af9bfc2ce3bc695bc3a68
June Eguilos <tulcusss@gmail.com>
2025-08-03 03:50:12 -0700

    nvm back to ts
    getting transformers js set up in nextjs

---
+++ doodlefight/next.config.ts
@@ -0,0 +1,11 @@
+// @ts-nocheck
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // https://nextjs.org/docs/app/api-reference/config/next-config-js/output
+  output: "standalone",
+  // https://nextjs.org/docs/app/api-reference/config/next-config-js/serverExternalPackages
+  serverExternalPackages: ["@huggingface/transformers"],
+};
+
+export default nextConfig;
